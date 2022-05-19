482c0f4a87251509fcef1685f18c688e76fe8188
Derricka S <derrickas728@gmail.com>
2022-05-18 20:16:35 -0400

    added param route

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ function filterByQuery(query, animalsArray) {
     
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     
@@ -46,6 +51,16 @@ app.get('/api/animals', (req, res) => {
     }
 
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req,res) => {
+    const result = findById(req.params.id, animals);
+    if(result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+    
 });
 
 app.listen(PORT, () => {
